4c15afcbc4840be8e378a838690d6f15929d270e
Arshdeep kaur <akaur501@my.bcit.ca>
2023-04-24 17:26:42 -0700

    checking git repo

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import render_template
 
+#a flask application template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
