7e41567ac82c2629161c61aa5ca15e6b00e91ae2
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2023-04-25 22:52:25 -0700

    add auth support and modularize the app

---
+++ myapp/database/create_tables.py
@@ -0,0 +1,8 @@
+from myapp import create_app
+from myapp.database.db import db
+
+app = create_app()
+
+# Adding this block to set up an application context
+with app.app_context():
+    db.create_all()
