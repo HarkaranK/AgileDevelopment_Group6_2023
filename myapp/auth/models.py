010394a6748bc0a62d9c9ed349ea73d30dcbb243
Arshdeep kaur <akaur501@my.bcit.ca>
2023-04-28 16:23:03 -0700

    changes

--- myapp/auth/models.py
+++ myapp/auth/models.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 from myapp.database.db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -8,23 +5,12 @@ class User(db.Model):
     id = db.Column(db.String(100), primary_key=True)
     password_hash = db.Column(db.String(128), nullable=False)
     user_type = db.Column(db.String(20), nullable=False)
-=======
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
+    
 class User:
     users = {'user1': {'password': 'password1', 'type': 'professor'}, 'user2': {'password': 'password2', 'type': 'student'}}
 
     def __init__(self, id):
         self.id = id
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
 
     def is_authenticated(self):
         return True
@@ -38,9 +24,6 @@ class User:
     def get_id(self):
         return self.id
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     @staticmethod
     def hash_password(password):
         return generate_password_hash(password)
@@ -58,11 +41,6 @@ class User:
         if user is None or not user.check_password(password):
             return None
         return user
-=======
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
     @classmethod
     def get(cls, user_id):
         if user_id not in cls.users:
@@ -74,10 +52,3 @@ class User:
         if user_id not in cls.users or cls.users[user_id]['password'] != password:
             return None
         return cls(user_id)
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
-=======
->>>>>>> 97281a16867be91c438e72be78e8cbbdac1a0671
