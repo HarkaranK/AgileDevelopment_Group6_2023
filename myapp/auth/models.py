7e8adcd7f1301633b37500788da3f03e362b88a8
Arshdeep kaur <akaur501@my.bcit.ca>
2023-04-29 10:18:29 -0700

    added more content to index.html

--- myapp/auth/models.py
+++ myapp/auth/models.py
@@ -3,14 +3,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.String(80), primary_key=True)
-    password = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)  # Change the field name to password_hash
     user_type = db.Column(db.String(20), nullable=False)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)  # Update the field name here as well
+        
+    @staticmethod
+    def hash_password(password):
+        return generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password_hash, password)  # Update the field name here as well
 
     def is_authenticated(self):
         return True
