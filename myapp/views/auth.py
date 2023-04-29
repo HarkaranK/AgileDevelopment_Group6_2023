f8735a9103a229bb89f5b6a43a132a3df7e44657
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2023-04-28 18:57:25 -0700

    add instance folder to gitignore

--- myapp/views/auth.py
+++ myapp/views/auth.py
@@ -1,11 +1,4 @@
 from flask import render_template, redirect, url_for, request, flash
-
-from flask import render_template, redirect, url_for, request
-
-from flask import render_template, redirect, url_for, request
-
-from flask import render_template, redirect, url_for, request
-
 from flask_login import login_user, logout_user, login_required, current_user
 from myapp.auth.models import User
 from myapp.database.models import Quiz, Question, Answer
