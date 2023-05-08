96163d49b9cd2295cce51f6b717a538374474981
rogerzhuang <87434722+rogerzhuang@users.noreply.github.com>
2023-05-08 13:59:42 -0700

    change db model back to original

--- myapp/database/models.py
+++ myapp/database/models.py
@@ -17,6 +17,7 @@ class Quiz(db.Model):
     quiz_name = db.Column(db.String(100), nullable=False)
     duration = db.Column(db.Integer, nullable=False)
     quiz_questions = db.relationship('QuizQuestion', backref='quiz', lazy=True)
+    participants = db.relationship('QuizParticipant', backref='quiz', lazy=True)
 
 
 class QuizQuestion(db.Model):
@@ -32,8 +33,17 @@ class Answer(db.Model):
     is_correct = db.Column(db.Boolean, nullable=False)
 
 
+class QuizParticipant(db.Model):
+    participation_id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.quiz_id'), nullable=False)
+    participant_id = db.Column(db.String(36), db.ForeignKey('user.user_id'), nullable=False)  # Updated ForeignKey
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_time = db.Column(db.DateTime, nullable=True)
+    responses = db.relationship('UserResponse', backref='participation', lazy=True)
+
+
 class UserResponse(db.Model):
     response_id = db.Column(db.Integer, primary_key=True)
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.quiz_id'), nullable=False)
+    participation_id = db.Column(db.Integer, db.ForeignKey('quiz_participant.participation_id'), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey('question.question_id'), nullable=False)
     answer_id = db.Column(db.Integer, db.ForeignKey('answer.answer_id'), nullable=False)
